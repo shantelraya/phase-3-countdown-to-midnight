da6bc6458ccef6ed63c81d15338a0bb1c7b7b41f
shantelraya <parischax@gmail.com>
2022-10-19 17:20:38 +0300

    done assignemnst

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(numba)
+    while numba > 0
+        puts "#{numba} SECOND(S)!"
+        numba -= 1
+    end
+    'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(numba)
+    while numba > 0
+        puts "#{numba} SECOND(S)!"
+        sleep(1)
+        numba -= 1
+    end
+    'HAPPY NEW YEAR!'
 end
